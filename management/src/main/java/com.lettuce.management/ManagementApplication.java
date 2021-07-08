9f67b0dfa0cf67442a186d7b5d30dad46269c362
Hosmos <375938415@qq.com>
2021-07-08 18:43:16 +0800

    新建后台管理系统module

---
+++ management/src/main/java/com.lettuce.management/ManagementApplication.java
@@ -0,0 +1,37 @@
+package com.lettuce.management;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Code is far away from bug with the animal protected
+ * 　┏┓　　  ┏┓
+ * ┏┻┻━━━┻┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┣┛
+ * 　　┗┳┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @author Hosmos
+ * @description
+ * @date 2021年07月08日
+ */
+@SpringBootApplication
+@MapperScan("com.lettuce.management.dao")
+public class ManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+}
