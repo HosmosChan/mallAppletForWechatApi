da16bab80a3ccf25ea726ac63113af9d09590ba4
Hosmos <375938415@qq.com>
2021-08-23 21:03:07 +0800

    代码生成功能（暂定）

---
+++ management/src/main/java/com.lettuce.management/controller/ManagementGenerateController.java
@@ -0,0 +1,67 @@
+package com.lettuce.management.controller;
+
+import com.lettuce.management.annotation.LogAnnotation;
+import com.lettuce.management.dto.BeanField;
+import com.lettuce.management.dto.GenerateDetail;
+import com.lettuce.management.dto.GenerateInput;
+import com.lettuce.management.service.ManagementGenerateService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * Code is far away from bug with the animal protected
+ * 　┏┓　　  ┏┓
+ * ┏┻┻━━━┻┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┣┛
+ * 　　┗┳┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @author Hosmos
+ * @description 代码生成controller层
+ * @date 2021年08月23日
+ */
+@Api(value = "generate", tags = "代码生成")
+@RestController
+@RequestMapping("/generate")
+public class ManagementGenerateController {
+    private static final Logger logger = LoggerFactory.getLogger(ManagementLoginController.class);
+    @Resource
+    private ManagementGenerateService managementGenerateService;
+    @ApiOperation("根据表名显示表信息")
+    @GetMapping(params = {"tableName"})
+    @RequiresPermissions("generate:edit")
+    public GenerateDetail generateByTableName(String tableName) {
+        GenerateDetail detail = new GenerateDetail();
+        detail.setBeanName(managementGenerateService.upperFirstChar(tableName));
+        List<BeanField> fields = managementGenerateService.listBeanField(tableName);
+        detail.setFields(fields);
+        return detail;
+    }
+
+    @LogAnnotation
+    @ApiOperation("生成代码")
+    @PostMapping
+    @RequiresPermissions("generate:edit")
+    public void save(@RequestBody GenerateInput input) {
+        managementGenerateService.saveCode(input);
+    }
+}
---
+++ management/src/main/java/com.lettuce.management/service/ManagementGenerateService.java
@@ -0,0 +1,61 @@
+package com.lettuce.management.service;
+
+import com.lettuce.management.dto.BeanField;
+import com.lettuce.management.dto.GenerateInput;
+
+import java.util.List;
+
+/**
+ * Code is far away from bug with the animal protected
+ * 　┏┓　　  ┏┓
+ * ┏┻┻━━━┻┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┣┛
+ * 　　┗┳┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @author Hosmos
+ * @description 代码生成业务层
+ * @date 2021年08月23日
+ */
+public interface ManagementGenerateService {
+    /**
+     * 获取数据库表信息
+     *
+     * @param tableName 数据库表名
+     * @return List<BeanField> 数据库表信息
+     * @author Hosmos
+     * @date 2021-08-23
+     */
+    List<BeanField> listBeanField(String tableName);
+
+    /**
+     * 转成驼峰并大写第一个字母
+     *
+     * @param string 欲转换的字符串
+     * @return String 转换后的字符串
+     * @author Hosmos
+     * @date 2021-08-23
+     */
+    String upperFirstChar(String string);
+
+    /**
+     * 根据输入内容生成代码
+     *
+     * @param input 输入的内容
+     * @author Hosmos
+     * @date 2021-08-23
+     */
+    void saveCode(GenerateInput input);
+}
