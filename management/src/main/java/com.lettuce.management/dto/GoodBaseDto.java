36fb4c068c85518379ed3d0838fda99266ef368b
Hosmos Chan <375938415@qq.com>
2021-08-28 08:00:58 +0800

    分類以及售賣狀態用字典下拉顯示

--- management/src/main/java/com.lettuce.management/dto/GoodBaseDto.java
+++ management/src/main/java/com.lettuce.management/dto/GoodBaseDto.java
@@ -22,21 +22,13 @@ import com.lettuce.management.entity.GoodBase;
  * 　　　┃┫┫　┃┫┫
  * 　　　┗┻┛　┗┻┛
  * 商品基础 dto
+ *
  * @author Hosmos
  * @date 2021年08月24日
  */
 public class GoodBaseDto extends GoodBase {
     private static final long serialVersionUID = 4599044873885533216L;
-    private String categoryName;
     private String goodPrice;
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 
     public String getGoodPrice() {
         return goodPrice;
